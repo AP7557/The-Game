e7de895791171fe7ec1bc0f4c44d32a00838d799
AP7557 <ap.akash.patel1999+github@gmail.com>
2021-02-19 20:00:57 +0000

    clean

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,9 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import './styles/Board.css';
 import { BoardBox } from './BoardBox.js'
+import io from 'socket.io-client';
+
+const socket = io(); // Connects to socket connection
 
 export function Board() {
     const [board, setBoard] = useState(Array(9).fill(null));
@@ -13,7 +16,20 @@ export function Board() {
             setOX(!OX)
         }
         setBoard(userClick)
+        socket.emit('chat', { message: userClick });
     }
+
+    useEffect(() => {
+        // Listening for a chat event emitted by the server. If received, we
+        // run the code in the function that is passed in as the second arg
+        socket.on('chat', (data) => {
+            console.log('Chat event received!');
+            console.log(data);
+            // If the server sends a message (on behalf of another client), then we
+            // add it to the list of messages to render it on the UI.
+            // setBoard(prevMessages => [...prevMessages, data.message]);
+        });
+    }, []);
     return (
         <div className="board" >
         {board.map((value, i)=><BoardBox value={value} onClickButton={()=>onClickButton(i)}/>)}
