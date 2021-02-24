40493d8983b10dccc04bda0b1f3dae842346b6bc
AP7557 <ap.akash.patel1999+github@gmail.com>
2021-02-24 18:13:16 +0000

    remake app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './styles/App.css';
-import { Board } from './Board.js'
 
 function App() {
   return (
     <div>
-      <Board/>
+      Tic-Tac-Toe
     </div>
   );
 }
