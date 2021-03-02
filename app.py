4ba8479aa72d4dd3dff8b601b0ed749311ff7d8e
AP7557 <ap.akash.patel1999+github@gmail.com>
2021-03-02 23:13:38 +0000

    trying to add db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, send_from_directory, json, session
+from flask import Flask, send_from_directory, json
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -8,9 +8,18 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv()) # This is to load your env variables from .env
 
 app = Flask(__name__, static_folder='./build/static')
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+import models
+db.create_all()
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
@@ -27,6 +36,12 @@ def index(filename):
 @socketio.on('connect')
 def on_connect():
     print('User connected!')
+    all_people = models.Person.query.all()
+    users = []
+    for person in all_people:
+        users.append(person.username)
+    print(users)
+    socketio.emit('user_list', {'users': users})
 
 # When a client disconnects from this Socket connection, this function is run
 @socketio.on('disconnect')
--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const socket = io(); // Connects to socket connection
 function App() {
     let [login, isLogin] = useState(false)
     let [currentUser, setCurrentUserName] = useState("")
+    const [userList, setUserList] = useState([]);
 
     const signin = (username) => {
         if (username != "") {
@@ -24,6 +25,11 @@ function App() {
     }
 
     useEffect(() => {
+        socket.on('user_list', (data) => {
+            console.log('User list event received!');
+            console.log(data);
+            setUserList(data.users);
+        });
         return () => {
             // This function will only be run when the component unmounts
             socket.emit('logout', { currentUser }) // your data here }});
@@ -31,6 +37,7 @@ function App() {
     })
     return (
         <div>
+            {userList.map((user) => <li> {user} </li>)}
             <h1 className="game"> Tic-Tac-Toe</h1>
             {currentUser && <h4 className="current">Your Username: {currentUser}</h4>}
             {login ?
